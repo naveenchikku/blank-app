7021a0a1110a03b761926ac9d78f718bc9990870
naveenchikku <138491481+naveenchikku@users.noreply.github.com>
2025-05-15 13:47:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,17 @@ st.markdown("""
     }
     </style>
 """, unsafe_allow_html=True)
+# Set constants
+infra_model = None
+first_integration = None
+tps = None
+infra_model = None
+storage_offered = None
+no_of_fields = None
+mapping_required = False
+mapping_complexity = None
+events_per_second = None
+payload_kb = None
 
 # Title and description
 st.title("🔮 Integration Fabric API Onboarding Cost Calculator")
@@ -52,13 +63,27 @@ st.markdown("### Forecast your onboarding costs with ease!")
 # Input form
 st.subheader("Input Parameters")
 integration_pattern = st.selectbox("Integration Pattern", ["Synchronous", "Asynchronous", "Event-based"])
-if integration_pattern != "Event-based":
-    int_complexity = st.selectbox("Integration Complexity", ["Simple", "Medium", "Complex"])
+
+if integration_pattern == "Synchronous":
+    tps = st.slider("Expected TPS", 1, 50, 2,2)
+    # events_per_month = st.slider("Expected requests per month",0,200000000,50,1000)
+    payload_kb = st.number_input("Payload size (KB)", 1, 10000, 50,100)
     infra_model = st.selectbox("Infrastructure Model", ["Shared", "Dedicated"])
+elif integration_pattern == "Asynchronous":
+    events_per_second = st.slider("EventHub Ingress (events/second)",0,2000,50,25)
+    storage_offered = st.slider("Storage offered(GB)",0,1000,50,50)
+    infra_model = st.selectbox("Infrastructure Model", ["Shared", "Dedicated"])
+    payload_kb = st.number_input("Payload size (KB)", 1, 1000, 50, 100)
 else:
-    int_complexity = None
-    infra_model = None
-oper_complexity = st.selectbox("Operational Complexity", ["Simple", "Medium", "Complex"])   
+    events_per_second = st.slider("Events/second)",0,100,10,10)
+    payload_kb = st.number_input("Payload size (KB)", 1, 10000, 50, 100)
+    storage_offered = st.slider("Storage offered(GB)",0,1000,50,50)
+# # pass_through_integration = st.checkbox("Pass through Integration?")
+# if not pass_through_integration:
+mapping_required = st.checkbox("Data transformation required?")
+if mapping_required:
+    no_of_fields = st.slider("No of fields",0,1000,2,10)
+    mapping_complexity = st.selectbox("Transformation complexity", ["Simple", "Medium", "Complex"])   
 
 if infra_model == "Dedicated":
     first_integration = st.checkbox("First Integration?")
@@ -69,92 +94,27 @@ submitted = st.button("💰 Forecast Cost")
 
 # Mock response and output
 if submitted:
-    params = {
+    payload = {
         "integration_pattern": integration_pattern,
-        "integration_complexity": int_complexity,
-        "operational_complexity": oper_complexity,
         "infrastructure_model": infra_model,
-        "first_integration": first_integration
+        "first_integration": first_integration,
+        "tps" :tps,
+        "infra_model" : infra_model,
+        "storage_offered" : storage_offered,
+        "no_of_fields":no_of_fields,
+        "mapping_required": mapping_required,
+        "mapping_complexity": mapping_complexity,
+        "events_per_second": events_per_second,
+        "payload_kb":payload_kb
     }
-
+    print(payload)
     try:
         # Call backend api
-        response = requests.post(url="https://func-costmeter.azurewebsites.net/api/cost-meter-poc",params=params)
+        response = requests.post(url="https://func-costmeter.azurewebsites.net/api/cost-meter-poc",json=payload)
         response.raise_for_status()
 
         if response.status_code == 200:
             result = response.json()
-            # result = {
-            #     "infra_cost_per_month": 84.18,
-            #     "infra_cost_per_year": 1010.12,
-            #     "operational_cost": 5791.16,
-            #     "total_cost": 6801.28,
-            #     "azure_resource_costs": {
-            #         "Function App ASP": 38.7,
-            #         "API Management": 9.64,
-            #         "Application Gateway": 1.01,
-            #         "PostgreSQL Database": 7.92,
-            #         "Blob storage ": 4.84,
-            #         "Queue Storage": 0.34,
-            #         "App insight": 6.96,
-            #         "Redis cache": 6.54,
-            #         "Linux Agent VM": 1.28
-            #     },
-            #     "tps_supported": 15,
-            #     "storage_offered": "N/A",
-            #     "operational_cost_breakdown": [
-            #         {
-            #             "Resource": "Dev -1",
-            #             "Resource Type": "IN_140",
-            #             "Effort in Days": 5
-            #         },
-            #         {
-            #             "Resource": "SVT -1",
-            #             "Resource Type": "IN_140",
-            #             "Effort in Days": 4
-            #         },
-            #         {
-            #             "Resource": "PO-1",
-            #             "Resource Type": "IN_150",
-            #             "Effort in Days": 1
-            #         },
-            #         {
-            #             "Resource": "PM/SM-1",
-            #             "Resource Type": "IN_150",
-            #             "Effort in Days": 1
-            #         },
-            #         {
-            #             "Resource": "PdM",
-            #             "Resource Type": "US_170",
-            #             "Effort in Days": 1
-            #         },
-            #         {
-            #             "Resource": "DL",
-            #             "Resource Type": "IN_160",
-            #             "Effort in Days": 2
-            #         },
-            #         {
-            #             "Resource": "UAT Support",
-            #             "Resource Type": "IN_140",
-            #             "Effort in Days": 3
-            #         },
-            #         {
-            #             "Resource": "Architect",
-            #             "Resource Type": "IN_150",
-            #             "Effort in Days": 2
-            #         },
-            #         {
-            #             "Resource": "Dev Manager",
-            #             "Resource Type": "IN_150",
-            #             "Effort in Days": 2
-            #         },
-            #         {
-            #             "Resource": "SVT Manager",
-            #             "Resource Type": "IN_150",
-            #             "Effort in Days": 2
-            #         }
-            #     ]
-            # }
             # Display metrics
             st.subheader("Cost Forecast")
             col1, col2, col3, col4 = st.columns(4)
@@ -165,15 +125,10 @@ if submitted:
 
             # Display Azure resource costs
             st.subheader("Azure Resource Costs")
-            df = pd.DataFrame.from_dict(result["azure_resource_costs"], orient="index", columns=["Cost (USD)"])
+            df = pd.DataFrame.from_dict(result["azure_resource_costs"], orient="index", columns=["Cost (USD) /month"])
             st.bar_chart(df)
             st.dataframe(df)
 
-            # Display additional metrics
-            st.subheader("Additional Information")
-            col5, col6 = st.columns(2)
-            col5.metric(label="📊 TPS Supported", value=f"{result['tps_supported']}")
-            # col6.metric(label="📊 Storage Offered", value=f"{result['storage_offered']}")
 
             # Display operational cost breakdown
             st.subheader("Operational Resources Breakdown")
